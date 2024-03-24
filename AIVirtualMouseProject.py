65dac9dba2bc4a1e843fccb1bce1f8b3faae39b6
Bethvour <bethvourc@gmail.com>
2024-03-23 21:45:26 -0500

    Mouse improvement

--- AIVirtualMouseProject.py
+++ AIVirtualMouseProject.py
@@ -1,73 +1,57 @@
 import cv2
 import numpy as np
-import HandTrackingModule as htm
-import time
 import pyautogui
 
-##########################
-wCam, hCam = 640, 480
-frameR = 100 # Frame Reduction
-smoothening = 7
-#########################
+class ColorHandDetector:
+    def __init__(self, hsv_lower, hsv_upper):
+        self.hsv_lower = hsv_lower
+        self.hsv_upper = hsv_upper
 
-pTime = 0
-plocX, plocY = 0, 0
-clocX, clocY = 0, 0
+    def findHands(self, img, draw=True):
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(imgHSV, self.hsv_lower, self.hsv_upper)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        bboxList = []
+        if contours:
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > 500:  # Minimum area to consider
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    bboxList.append((x, y, w, h))
+                    if draw:
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        return img, bboxList
 
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-detector = htm.handDetector(maxHands=1)
-wScr, hScr = pyautogui.screen.size()
-# print(wScr, hScr)
+def main():
+    cap = cv2.VideoCapture(0)  # Adjust as per your webcam setup
+    detector = ColorHandDetector(hsv_lower=np.array([36, 25, 25]), hsv_upper=np.array([86, 255, 255]))
+    wScr, hScr = pyautogui.size()
 
-while True:
-    # 1. Find hand Landmarks
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
-    # 2. Get the tip of the index and middle fingers
-    if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-        # print(x1, y1, x2, y2)
-    
-    # 3. Check which fingers are up
-    fingers = detector.fingersUp()
-    # print(fingers)
-    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-    (255, 0, 255), 2)
-    # 4. Only Index Finger : Moving Mode
-    if fingers[1] == 1 and fingers[2] == 0:
-        # 5. Convert Coordinates
-        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-        # 6. Smoothen Values
-        clocX = plocX + (x3 - plocX) / smoothening
-        clocY = plocY + (y3 - plocY) / smoothening
-    
-        # 7. Move Mouse
-        pyautogui.mouse.move(wScr - clocX, clocY)
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        plocX, plocY = clocX, clocY
-        
-    # 8. Both Index and middle fingers are up : Clicking Mode
-    if fingers[1] == 1 and fingers[2] == 1:
-        # 9. Find distance between fingers
-        length, img, lineInfo = detector.findDistance(8, 12, img)
-        print(length)
-        # 10. Click mouse if distance short
-        if length < 40:
-            cv2.circle(img, (lineInfo[4], lineInfo[5]),
-            15, (0, 255, 0), cv2.FILLED)
-            pyautogui.mouse.click()
-    
-    # 11. Frame Rate
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-    (255, 0, 0), 3)
-    # 12. Display
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    while True:
+        success, img = cap.read()
+        img, bboxList = detector.findHands(img)
+
+        if bboxList:
+            # For simplicity, just use the first bounding box
+            x, y, w, h = bboxList[0]
+            # Calculate the center of the bounding box
+            cx, cy = x + w // 2, y + h // 2
+
+            # Convert coordinates to screen size
+            screenX = np.interp(cx, (0, 640), (0, wScr))
+            screenY = np.interp(cy, (0, 480), (0, hScr))
+            pyautogui.moveTo(screenX, screenY)
+
+            # Simple click mechanism: click if the bounding box area is small
+            if w * h < 25000:  # Threshold might need adjustment
+                pyautogui.click()
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
